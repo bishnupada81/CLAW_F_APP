56724ad705f95392dc31c61c9f07f8aa0cd623c0
bishnupada81 <ayerish9596@yahoo.com>
2024-02-19 18:49:49 +0530

    Set pin page done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SetPinComponent } from './component/set-pin/set-pin.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { CameraComponent } from './component/camera/camera.component';
+import { MatDialogModule } from '@angular/material';
+import { PopUpComponent } from './component/pop-up/pop-up.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SetPinComponent
+    SetPinComponent,
+    CameraComponent,
+    PopUpComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    MatDialogModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/component/set-pin/set-pin.component.ts
+++ src/app/component/set-pin/set-pin.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validator, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { MatDialog } from '@angular/material';
+import { PopUpComponent } from '../pop-up/pop-up.component';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-set-pin',
@@ -6,10 +11,52 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./set-pin.component.css']
 })
 export class SetPinComponent implements OnInit {
+  formModal:any;
 
-  constructor() { }
+  constructor(private router: Router, public dialogRef:MatDialog) { }
 
   ngOnInit(): void {
   }
 
+  public setPinForm = new FormGroup({
+    phoneNumber: new FormControl("",
+         [Validators.required,
+         Validators.pattern('^[0-9]*$'),
+         Validators.maxLength(10),
+         Validators.minLength(10)]
+         ),
+    setPin: new FormControl("",
+        [Validators.required,
+        Validators.pattern('^[0-9]*$'),
+        Validators.maxLength(6),
+        Validators.minLength(6)]
+        )
+  })
+
+  openDialog(){
+    this.dialogRef.open(PopUpComponent)
+  }
+
+  public SetPinFormSubmit() {
+    // console.log(this.setPinForm.value);
+    if(this.setPinForm.value){
+      Swal.fire({
+        title: "Your Pin successfully set",
+        confirmButtonText: "Okay",
+      }).then((result) => {
+
+        this.router.navigateByUrl('/camera');
+
+      });
+    }
+
+  }
+
+  get phoneNumber() {
+    return this.setPinForm.get('phoneNumber');
+  }
+
+  get setPin() {
+    return this.setPinForm.get('setPin');
+  }
 }
